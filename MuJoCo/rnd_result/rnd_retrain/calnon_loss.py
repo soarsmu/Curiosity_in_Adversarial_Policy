8562b6648f2ce18fda468f09652b6a8b14d688d2
Chen <49557610+2019ChenGong@users.noreply.github.com>
2022-01-20 15:55:26 +0800

    Add files via upload

---
+++ MuJoCo/rnd_result/rnd_retrain/calnon_loss.py
@@ -0,0 +1,39 @@
+import numpy as np
+import os
+envs = ['KickAndDefend', 'SumoAnts', 'RunToGoalAnts', 'YouShallNotPass', 'SumoHumans' ]
+algos = ['our attack'] # 'attack1 ICLR', 'attack2 ICML',
+tie_prefix = 'retrain_tie'
+win_prefix = 'retrain_win'
+non_loss_prefix = './retrain_nonloss'
+os.mkdir(non_loss_prefix)
+for env in envs: 
+    os.mkdir(non_loss_prefix +'/' + env +'/')
+
+for env in envs: 
+    for algo in algos:
+        os.mkdir(non_loss_prefix +'/' + env +'/' + algo)
+
+for env in envs:
+    for algo in algos:
+        for _, _, files in os.walk(tie_prefix + '/' + env+'/' + algo):
+            break
+        for file in files:
+            tie = []
+            win = []
+            non_loss = [] 
+            with open(tie_prefix + '/' + env +'/' + algo +'/' + file) as f:
+                data = f.readlines()
+            for line in data[1:]:
+                tie.append(float(line.strip(' ').strip('\n').strip(' ').split(',')[-1]))
+            with open(win_prefix + '/' + env +'/' + algo +'/' + file) as f:
+                data = f.readlines()
+            for line in data[1:]:
+                win.append(float(line.strip(' ').strip('\n').strip(' ').split(',')[-1]))
+            for i in range(len(win)):
+                non_loss.append(win[i] + tie[i])
+            with open(non_loss_prefix +'/' + env + '/' + algo +'/' + file, 'a+') as f:
+                f.write('title')
+                f.write('\n')
+                for c in non_loss:
+                    f.write(',,{}'.format(c))
+                    f.write('\n')
